218f77bfaea87eeeb6a63efcf0c2fc8e933787b8
anna <anna.nouchi@epkotsoftware.co.jp>
2023-05-07 19:49:00 +0900

    housing_loan_chartsのマイグレーション修正

--- root/database/migrations/2023_04_26_120813_create_housing_loan_charts_table.php
+++ root/database/migrations/2023_04_26_120813_create_housing_loan_charts_table.php
@@ -15,8 +15,12 @@ return new class extends Migration
     {
         Schema::create('housing_loan_charts', function (Blueprint $table) {
             $table->id()->comment('ID');
+            $table->string('name')->comment('名前');
             $table->string('usage_situation')->comment('利用状況');
-            $table->integer('number')->comment('カウント');
+            $table->string('financial_institution')->comment('住宅金融公庫');
+            $table->string('financial_institution2')->comment('地方銀行');
+            $table->string('financial_institution3')->comment('みずほ銀行');
+            $table->string('financial_institution4')->comment('その他');
             $table->softDeletes()->comment('削除日時'); // deleted_at
             $table->timestamp('created_at')->nullable()->comment('作成日時'); // created_at
             $table->timestamp('updated_at')->nullable()->comment('更新日時'); // updated_at
