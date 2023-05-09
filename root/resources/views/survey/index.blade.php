8773f244b8f6ce8a17433fb130e98adee2950b5e
anna <anna.nouchi@epkotsoftware.co.jp>
2023-05-09 18:20:04 +0900

    集計結果のindex棒グラフ(借入先)実装

--- root/resources/views/survey/index.blade.php
+++ root/resources/views/survey/index.blade.php
@@ -22,7 +22,7 @@
     <div class="container p-3">
 
         <div style="border: 1px solid;">
-            <p class="fs-4 fw-bold ps-4 mt-2">1,住宅ローンの利用有無</p>
+            <p class="fs-4 fw-bold ps-4 mt-2">1.住宅ローンの利用有無</p>
             <p class="ps-4 mb-0">[あなたご自身、又は配偶者の方は、住宅ローンを借りていますか?]</p>
             <div style="width: 30%;" class="mx-auto">
                 <canvas id="usageSituation"></canvas>
@@ -31,6 +31,21 @@
 
     </div>
     <!-- Content end -->
+    <!--2,住宅ローンの借入先棒グラフ-->
+    <div class="container p-3">
+
+        <div style="border: 1px solid;">
+            <p class="fs-4 fw-bold ps-4 mt-2">2.住宅ローンの借入先</p>
+            <p class="ps-4 mb-0">(住宅ローンのある方へ)住宅ローンの借入先はどちらの金融機関ですか?[複数回答]</p>
+            <div style="width: 70%;" class="mx-auto">
+                <canvas id="financialInstitution"></canvas>
+            </div>
+        </div>
+
+    </div>
+    <!-- Content end -->
+
+
     <!-- chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.0/dist/chart.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0 "></script>
@@ -77,6 +92,73 @@
                     }
                 }
             };
+            //グラフ作成
+            new Chart(ctx, config);
+        })();
+
+        //住宅ローンの借入先の棒グラフ
+        (() => {
+            //データをJS用に変換
+            const data = {{Js::from($financialInstitutionList)}};
+
+            //canvasの値取得
+            const ctx = document.getElementById('financialInstitution');
+
+            //円グラフの設定
+            const config = {
+                type: 'bar',
+                data: {
+                    labels: ['住宅金融公庫', '地方銀行', 'みずほ銀行', 'その他'],
+                    datasets: [{
+                        label: '住宅ローンの借入先',
+                        data: data,
+                        backgroundColor: [
+                            "rgba(126,162,255,0.7)",
+                            "rgba(126,162,255,0.7)",
+                            "rgba(126,162,255,0.7)",
+                            "rgba(126,162,255,0.7)",
+                        ],
+                    }]
+                },
+                plugins: [ChartDataLabels], // pluginとしてchartdatalabelsを追加
+                options: {
+                    indexAxis: 'y',
+                    scales: {
+                        x: {
+                            min: 0,
+                            max: 100,
+                            ticks: {
+                                stepSize: 10,
+                                font: {
+                                    size: 18,
+                                    weight:'bold'
+                                }
+                            },
+                        },
+                        y: {
+                            ticks: {
+                                font: {
+                                    size: 20,
+                                    weight:'bold'
+                                }
+                            }
+                        }
+                    },
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                        datalabels: {
+                            formatter: function(value, context) {
+                                return value + '%';
+                            }
+                        }
+                    }
+                }
+            };
+
+
+
             //グラフ作成
             new Chart(ctx, config);
         })();
