4a9175874f77e38f95a384fe77803b38e37bc877
anna <anna.nouchi@epkotsoftware.co.jp>
2023-05-22 16:29:59 +0900

    page3を問1の回答によって戻るの遷移先が変わるように修正

--- root/resources/views/survey/question-page/page3.blade.php
+++ root/resources/views/survey/question-page/page3.blade.php
@@ -1,6 +1,7 @@
 @extends('survey.base')
 
 @section('content')
+<!-- プログレスバー -->
 <div class="position-relative m-4">
   <div class="progress" style="height: 3px;">
     <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
@@ -9,9 +10,12 @@
   <button type="button" class="position-absolute top-0 start-50 translate-middle btn btn-sm btn-success rounded-pill" style="width: 2rem; height:2rem;">2</button>
   <button type="button" class="position-absolute top-0 start-100 translate-middle btn btn-sm btn-secondary rounded-pill" style="width: 4rem; height:2rem;">完了</button>
 </div>
+<!-- ENDプログレスバー -->
+
 <h1>お名前を記入してください。</h1>
 
 <div class="row my-4">
+
   <form method="post" action="{{ route('housing-loan.question-page.page3.submitForm') }}">
     @csrf
 
@@ -24,5 +28,11 @@
   </form>
 
 </div>
-<a href="#" class="btn btn-secondary">戻る</a>
+<!-- 問1の回答によって、戻る時遷移する場所を切り替えている -->
+@if(session('form.page1')=="借りたことがない")
+<a href="{{ route('housing-loan.question-page.page1.showPage1') }}" class="btn btn-secondary">戻る</a>
+@else
+<a href="{{ route('housing-loan.question-page.page2.showPage2') }}" class="btn btn-secondary">戻る</a>
+@endif
+
 @endsection
