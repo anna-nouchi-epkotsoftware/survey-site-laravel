440a5a39731b3506133aa13e79f375d05bc245cb
anna <anna.nouchi@epkotsoftware.co.jp>
2023-05-24 11:31:43 +0900

    question-pageのindexのview修正

--- root/resources/views/survey/question-page/index.blade.php
+++ root/resources/views/survey/question-page/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<p class="fs-5">回答時間の目安：　　分</p>
+<p class="fs-5">回答時間の目安： 1分</p>
 <p class="fs-5 border-bottom border-secondary pb-5">住宅ローンに関する簡単なアンケートになります。<br />
     ご協力の程をお願いいたします。
 </p>
